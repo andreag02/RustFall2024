8110544667a5aca32029104bdd72089f8cb9cf12
Andrea G <126801118+andreag02@users.noreply.github.com>
2024-12-04 05:42:52 +0000

    Assignment 2 Completed

--- Assignments/SimpleConcurrency/Assignment2/src/main.rs
+++ Assignments/SimpleConcurrency/Assignment2/src/main.rs
@@ -1,3 +1,26 @@
+use std::thread;
+use std::sync::{Arc, Mutex};
+
 fn main() {
-    println!("Hello, world!");
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = Vec::new();
+
+    for _i in 0..5{
+        let counter = Arc::clone(&counter);
+
+        let handle = thread::spawn(move || {
+            for _j in 0..10{
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles{
+        handle.join().unwrap();
+    }
+
+    println!("Final counter: {}", *counter.lock().unwrap());
 }
