9ab555176880e1b46983f0bf6de5b1c4991498cc
Andrea G <126801118+andreag02@users.noreply.github.com>
2024-12-04 05:11:50 +0000

    Assignment 1 Completed

--- Assignments/SimpleConcurrency/Assignment1/src/main.rs
+++ Assignments/SimpleConcurrency/Assignment1/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+
+fn main(){
+    let mut handles = Vec::new();
+
+    for i in 1..4{
+        let handle = thread::spawn(move || {
+            println!("Thread {}", i);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles{
+        handle.join().unwrap();
+    }
+
+    println!("All threads completed.");
 }
